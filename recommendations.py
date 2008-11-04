c4e2d6cbba57cb05f7114fda07efea9f569067b3
Nicolas Weber <thakis@dhcp-172-19-7-141.corp.google.com>
2008-11-04 10:04:10 -0800

    added pearson method

--- recommendations.py
+++ recommendations.py
@@ -20,3 +20,19 @@ def sim_pearson(prefs, person1, person2):
 
   n = len(ci)
   if n == 0: return 0
+
+  sum1 = sum([prefs[person1][it] for it in ci])
+  sum2 = sum([prefs[person2][it] for it in ci])
+
+  sqSum1 = sum([pow(prefs[person1][it], 2) for it in ci])
+  sqSum2 = sum([pow(prefs[person2][it], 2) for it in ci])
+
+  pSum = sum([prefs[person1][it] * prefs[person2][it] for it in ci])
+
+  num = pSum - (sum1*sum2/n)
+  den = sqrt((sqSum1 - pow(sum1, 2)/n) * (sqSum2 - pow(sum2, 2)/n))
+  if den == 0:
+    if num == 0: return 1  # all points equal
+    else: return 0
+
+  return num/den
--- recommendations_test.py
+++ recommendations_test.py
@@ -2,34 +2,54 @@ import unittest
 
 import recommendations
 
-class SimDistanceTestCase(unittest.TestCase):
-
-  def testNormal(self):
-    prefs = { 'Nico': {'h': 0.8, 'b':0.2}, 'Yann': {'h': 0.4, 'b':0.1}}
-    self.assertAlmostEquals(0.7080596,
-        recommendations.sim_distance(prefs, 'Nico', 'Yann'))
-
-  def testAdditionalLeft(self):
-    prefs = { 'Nico': {'h': 0.8, 'b':0.2, 'c':0.9}, 'Yann': {'h': 0.4, 'b':0.1}}
-    self.assertAlmostEquals(0.7080596,
-        recommendations.sim_distance(prefs, 'Nico', 'Yann'))
-
-  def testAdditionalRight(self):
-    prefs = { 'Nico': {'h': 0.8, 'b':0.2}, 'Yann': {'h': 0.4, 'b':0.1, 'c':0.9}}
-    self.assertAlmostEquals(0.7080596,
-        recommendations.sim_distance(prefs, 'Nico', 'Yann'))
+class DistanceTestCase:
 
   def testIdentical(self):
-    prefs = { 'Nico': {'h': 1}, 'Yann': {'h': 1}}
-    self.assertEquals(1.0, recommendations.sim_distance(prefs, 'Nico', 'Yann'))
+    prefs = { 'Nico': {'h': 1, 'b':0.4}, 'Yann': {'h': 1, 'b': 0.4}}
+    self.assertEquals(1.0, self.metric(prefs, 'Nico', 'Yann'))
+
+  def testOneEqualElement(self):
+    prefs = { 'Nico': {'h': 0.9}, 'Yann': {'h': 0.9}}
+    self.assertEquals(1.0, self.metric(prefs, 'Nico', 'Yann'))
 
   def testEmptyPrefs(self):
     prefs = { 'Nico': {}, 'Yann': {}}
-    self.assertEquals(0.0, recommendations.sim_distance(prefs, 'Nico', 'Yann'))
+    self.assertEquals(0.0, self.metric(prefs, 'Nico', 'Yann'))
 
   def testEmptyIntersection(self):
     prefs = { 'Nico': {'h': 1}, 'Yann': {'z': 1}}
-    self.assertEquals(0.0, recommendations.sim_distance(prefs, 'Nico', 'Yann'))
+    self.assertEquals(0.0, self.metric(prefs, 'Nico', 'Yann'))
+
+  def testAdditionalLeft(self):
+    addLeft = self.prefs.copy()
+    addLeft['Nico']['c'] = 0.9
+    self.assertAlmostEquals(self.metric(self.prefs, 'Nico', 'Yann'),
+        self.metric(addLeft, 'Nico', 'Yann'))
+
+  def testAdditionalRight(self):
+    addRight = self.prefs.copy()
+    addRight['Yann']['c'] = 0.9
+    self.assertAlmostEquals(self.metric(self.prefs, 'Nico', 'Yann'),
+        self.metric(addRight, 'Nico', 'Yann'))
+
+
+class SimDistanceTestCase(DistanceTestCase, unittest.TestCase):
+  def setUp(self):
+    self.metric = recommendations.sim_distance
+    self.prefs = { 'Nico': {'h': 0.8, 'b':0.2}, 'Yann': {'h': 0.4, 'b':0.1}}
+
+  def testNormal(self):
+    self.assertAlmostEquals(0.7080596, self.metric(self.prefs, 'Nico', 'Yann'))
+
+
+class SimPearsonTestCase(DistanceTestCase, unittest.TestCase):
+  def setUp(self):
+    self.metric = recommendations.sim_pearson
+    self.prefs = { 'Nico': {'h': 0.8, 'b':0.2}, 'Yann': {'h': 0.4, 'b':0.1}}
+
+  def testNormal(self):
+    self.assertAlmostEquals(1, self.metric(self.prefs, 'Nico', 'Yann'))
+
 
 
 if __name__ == '__main__':
