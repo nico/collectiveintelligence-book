3bbd08922d9397f1778b3e2c7497a44c26447bd8
Nicolas Weber <thakis@thakis-macbookpro.local>
2008-11-14 08:45:40 -0800

    complete first shot at feed downloader

--- generatefeedvector.py
+++ generatefeedvector.py
@@ -1,14 +1,18 @@
 import feedparser
+import collections
 import re
+
 
 def getwords(html):
   text = re.compile(r'<[^>]+>').sub('', html)
   words = re.compile(r'[^A-z^a-z]+').split(text)
   return [word.lower() for word in words if word]
 
+
 def getwordcounts(url):
   d = feedparser.parse(url)
-  wc = {}
+  print d
+  wc = collections.defaultdict(int)
 
   for e in d.entries:
     if 'summary' in e: summary = e.summary
@@ -18,3 +22,33 @@ def getwordcounts(url):
     for word in words:
       wc[word] += 1
   return d.feed.title, wc
+
+
+def main():
+  apcount = collections.defaultdict(int)
+  wordcounts = {}
+  for url in file('feedlist.txt'):
+    title, wc = getwordcounts(url)
+    wordcounts[title] = wc
+    for word, count in wc.iteritems():
+      if count > 1:
+        apcount[word] += 1
+
+  wordlist = []
+  for w, bc in apcount.iteritems():
+    frac = float(bc)/len(feedlist)
+    if 0.1 < frac < 0.5: wordlist.append(w)
+
+  out = file('blogdata.txt', 'w')
+  out.write('Blog')
+  for w in wordlist: out.write('\t' + w)
+  out.write('\n')
+  for blogname, counts in apcount:
+    out.write(blogname)
+    for w in wordlist:
+      if w in counts: out.write('\t' + w)
+      else: out.write('\t0')
+    out.write('\n')
+
+if __name__ == '__main__':
+  main()
