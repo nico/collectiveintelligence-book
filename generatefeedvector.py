ec73e6e07282894895990206023e80a4ff56bd85
Nicolas Weber <thakis@thakis-macbookpro.local>
2008-11-18 19:24:17 -0800

    bring generatefeedparser in a usable state, add its output

--- generatefeedvector.py
+++ generatefeedvector.py
@@ -21,13 +21,21 @@ def getwordcounts(url):
     words = getwords('%s %s' % (e.title, summary))
     for word in words:
       wc[word] += 1
+
+  if 'title' not in d.feed:
+    print 'Invalid url', url
+    return 'bogus data', wc
   return d.feed.title, wc
 
 
 def main():
+
+  # XXX: break this up into smaller funtions, write tests for them
+
   apcount = collections.defaultdict(int)
   wordcounts = {}
-  for url in file('feedlist.txt'):
+  feedlist = open('feedlist.txt').readlines()
+  for url in feedlist:
     title, wc = getwordcounts(url)
     wordcounts[title] = wc
     for word, count in wc.iteritems():
@@ -43,10 +51,10 @@ def main():
   out.write('Blog')
   for w in wordlist: out.write('\t' + w)
   out.write('\n')
-  for blogname, counts in apcount:
+  for blogname, counts in wordcounts.iteritems():
     out.write(blogname)
     for w in wordlist:
-      if w in counts: out.write('\t' + w)
+      if w in counts: out.write('\t%d' % counts[w])
       else: out.write('\t0')
     out.write('\n')
 
