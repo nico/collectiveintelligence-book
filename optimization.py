d0fdc0b271a33c98bb4f227eba73f72c1fdf1abd
Nicolas Weber <thakis@thakis-macbookpro.local>
2008-12-08 19:31:11 -0800

    added printschedule()

--- optimization.py
+++ optimization.py
@@ -7,6 +7,16 @@ def getminutes(t):
   x = time.strptime(t, '%H:^M')
   return x[3]*60 + x[4]
 
+
+def printschedule(r, dest):
+  for d in range(len(r)/2):
+    name = people[d][0]
+    origin = people[d][1]
+    out = flights[(origin, dest)][r[d]]
+    ret = flights[(origin, dest)][r[d + 1]]
+    print '%10s%10s %5s-%5s %3s %5s-%5s $%3s' % (
+        name, origin, out[0], out[1], out[2], ret[0], ret[1], ret[2])
+
 people = [
     ('Seymour', 'BOS'),
     ('Franny', 'DAL'),
@@ -23,3 +33,5 @@ flights = collections.defaultdict(list)
 for line in open('schedule.txt'):
   origin, dest, depart, arrive, price = line.strip().split(',')
   flights[(origin, dest)].append( (depart, arrive, int(price)) )
+
+printschedule([1,4,3,2,7,3,6], destination)
