9059f8919c733c80b1af552089ae3f27e7f30593
Nicolas Weber <thakis@thakis-macbookpro.local>
2008-12-08 19:25:35 -0800

    added schedule reading

--- optimization.py
+++ optimization.py
@@ -1,6 +1,11 @@
+import collections
 import time
 import random
 import math
+
+def getminutes(t):
+  x = time.strptime(t, '%H:^M')
+  return x[3]*60 + x[4]
 
 people = [
     ('Seymour', 'BOS'),
@@ -12,3 +17,9 @@ people = [
     ]
 
 destination = 'LGA'  # LaGuardia (New York)
+
+flights = collections.defaultdict(list)
+
+for line in open('schedule.txt'):
+  origin, dest, depart, arrive, price = line.strip().split(',')
+  flights[(origin, dest)].append( (depart, arrive, int(price)) )
