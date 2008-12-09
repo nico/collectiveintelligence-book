c33207c652025c4251bbcdf02df12a2c2fcbb28a
Nicolas Weber <thakis@thakis-macbookpro.local>
2008-12-08 19:20:09 -0800

    added optimization.py

---
+++ optimization.py
@@ -0,0 +1,14 @@
+import time
+import random
+import math
+
+people = [
+    ('Seymour', 'BOS'),
+    ('Franny', 'DAL'),
+    ('Zooey', 'CAK'),
+    ('Walt', 'MIA'),
+    ('Buddy', 'ORD'),
+    ('Les', 'OMA'),
+    ]
+
+destination = 'LGA'  # LaGuardia (New York)
