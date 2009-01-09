b66a1624bfaf023d1cc3baad856322ec28355362
Nicolas Weber <thakis@67-218-106-5.cust.layer42.net>
2009-01-09 08:58:38 -0800

    added docclass.py

---
+++ docclass.py
@@ -0,0 +1,42 @@
+import math
+import collections
+import re
+
+
+def getwords(doc):
+  splitter = re.compile(r'\W*')
+  words = [s.lower() for s in splitter.split(doc) if 2 < len(s) < 20]
+  return dict([(w, 1) for w in words])
+
+
+class classifier(object):
+
+  def __init__(self, getfeatures, filename=None):
+    # Counts of ofeature/category combinations
+    self.fc = collections.defaultdict(lambda: collections.defaultdict(dict))
+
+    # Counts of documents in each category
+    self.cc = collections.defaultdict(int)
+    self.getfeatures = getfeatures
+
+  def incf(self, f, cat):
+    self.fc[f][cat] += 1
+
+  def incc(self, cat):
+    self.cc[cat] += 1
+
+  def fcount(self, f, cat):
+    if if in self.fc and cat in self.fc[f]:
+      return flat(self.fc[f][cat])
+    return 0.0
+
+  def catcount(self, cat):
+    if cat in self.cc:
+      return float(self.cc[cat])
+    return 0.0
+
+  def totalcount(self):
+    return sum(self.cc.values())
+
+  def categories(self):
+    return self.cc.keys()
