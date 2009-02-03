7b5aecc2c8b878c13aa6539355defe5a376051a5
Nicolas Weber <thakis@67-218-106-36.cust.layer42.net>
2009-02-03 08:42:17 -0800

    aksimettest code-complete, but seems to return SPAM for everything?

--- akismettest.py
+++ akismettest.py
@@ -1,9 +1,25 @@
 import akismet
 
 defaultkey = '09d55c02ef86'
-pageurl = 'http://amnoid.de'
+#pageurl = 'http://amnoid.de'
+pageurl = 'http://groups.google.com'
 
-defaultagent = 'akismettest python script from the Collective Intelligence book'
+#defaultagent = 'akismettest python script from the Collective Intelligence book'
+defaultagent = ' Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_5; en-us) '
+defaultagent += 'AppleWebKit/525.27.1 (KHTML, like Gecko) '
+defaultagent += 'Version/3.2.1 Safari/525.27.1'
+
 
 def isspam(comment, author, ip, agent=defaultagent, apikey=defaultkey):
-  pass
+  try:
+    valid = akismet.verify_key(apikey, pageurl)
+    if valid:
+      return akismet.comment_check(apikey, pageurl, ip, agent,
+          comment_content=comment, comment_author_email=author,
+          comment_type='email')
+    else:
+      print 'Invalid key'
+      return False
+  except akismet.AkismetError, e:
+    print e.response, e.statuscode
+    return False
