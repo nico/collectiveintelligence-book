cdc5c33162d0b6bac5f83e9b569d4a97227c36f1
Nicolas Weber <thakis@67-218-106-36.cust.layer42.net>
2009-02-04 08:36:44 -0800

    start with chapter 7; add demo main to akismettest

--- akismettest.py
+++ akismettest.py
@@ -5,7 +5,7 @@ defaultkey = '09d55c02ef86'
 pageurl = 'http://groups.google.com'
 
 #defaultagent = 'akismettest python script from the Collective Intelligence book'
-defaultagent = ' Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_5; en-us) '
+defaultagent = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_5; en-us) '
 defaultagent += 'AppleWebKit/525.27.1 (KHTML, like Gecko) '
 defaultagent += 'Version/3.2.1 Safari/525.27.1'
 
@@ -23,3 +23,9 @@ def isspam(comment, author, ip, agent=defaultagent, apikey=defaultkey):
   except akismet.AkismetError, e:
     print e.response, e.statuscode
     return False
+
+
+if __name__ == '__main__':
+  print isspam('Buy Viagra!', 'rz223@hotmail.com', '127.0.0.1')
+  print isspam("That's an interesting post. My vote goes to the green one",
+      'jwz@gmail.com', '67.218.106.36')
---
+++ treepredict.py
@@ -0,0 +1,1 @@
+my_data = [line.split('\t') for line in open('decision_tree_example.txt')]
